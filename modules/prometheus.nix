c1ab3f6fc5c29bcd27f66b0b9e418b1facd7b233
root <root@timmi-test>
2021-09-04 17:08:16 +0200

    blackbox exporter

--- modules/prometheus.nix
+++ modules/prometheus.nix
@@ -7,6 +7,10 @@
         enable = true;
         enabledCollectors = [ "systemd" "meminfo" "filesystem" "filefd" "textfile" ];
       };
+      blackbox = {
+        enable = true;
+        configFile = ./blackbox.yaml;  ## see https://github.com/prometheus/blackbox_exporter/blob/master/example.yml
+      };
     };
     scrapeConfigs = [
       {
@@ -14,6 +18,22 @@
         static_configs = [{
           targets = [ "127.0.0.1:${toString config.services.prometheus.exporters.node.port}" ];
         }];
+      }
+      {
+        job_name = "blackbox";
+        metrics_path = "/probe";
+        params = { module = [ "http_2xx" ]; };
+        static_configs = [{
+          targets = [
+            "127.0.0.1:3000"
+            "127.0.0.1:4000"
+          ];
+        }];
+        relabel_configs = [
+          { source_labels = [ "__address__" ]; target_label = "__param_target"; }
+          { source_labels = [ "__param_target" ]; target_label = "instance"; }
+          { target_label = "__address__"; replacement = "127.0.0.1:9115"; }
+        ];
       }
     ];
   };
