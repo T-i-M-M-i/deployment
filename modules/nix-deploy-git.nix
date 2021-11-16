81c27dcb6c8f1b5a3a401247b1e21f62c58cbc32
Johannes Lötzsch <git@johannesloetzsch.de>
2021-11-16 04:46:13 +0100

    added jenkins deploy keys

--- modules/nix-deploy-git.nix
+++ modules/nix-deploy-git.nix
@@ -3,7 +3,12 @@
 {
   services.nix-deploy-git = {
     enable = true;
-    keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW+YfsFtRz1h/0ubcKU+LyGfxH505yUkbWa5VtRFNWF2fjTAYGj6o5M4dt+fv1h370HXvvOBtt8sIlWQgMsD10+9mvjdXWhTcpnYPx4yWuyEERE1/1BhItrog6XJKAedbCDpQQ+POoewouiHWVAUfFByPj5RXuE8zKUeIEkGev/QKrKTLnTcS8zFs/yrokf1qYYR571B3U8IPDjpV/Y1GieG3MSNaefIMCwAAup1gPkUA0XZ4A1L7NdEiUEHlceKVu9eYiWUM+wDRunBXnLHubeGyP8KmBA7PNKgml3WWRNTZjqNQk4u9Bl+Qea5eCkD8KI257EqgXYXy0QBWNyF8X j03@l302" ];
+    keys = [ ## J03
+             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDW+YfsFtRz1h/0ubcKU+LyGfxH505yUkbWa5VtRFNWF2fjTAYGj6o5M4dt+fv1h370HXvvOBtt8sIlWQgMsD10+9mvjdXWhTcpnYPx4yWuyEERE1/1BhItrog6XJKAedbCDpQQ+POoewouiHWVAUfFByPj5RXuE8zKUeIEkGev/QKrKTLnTcS8zFs/yrokf1qYYR571B3U8IPDjpV/Y1GieG3MSNaefIMCwAAup1gPkUA0XZ4A1L7NdEiUEHlceKVu9eYiWUM+wDRunBXnLHubeGyP8KmBA7PNKgml3WWRNTZjqNQk4u9Bl+Qea5eCkD8KI257EqgXYXy0QBWNyF8X j03@l302"
+             ## JENKINS
+             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDkmeUjdrHVaegkV5uneVNsyop1D63EN+A9TTkAalt0AOpTIsFb/kBwtgB2gEHOJNCPmqnPHJCPVwBhM/5/PJ4Y3kKMk/NUk17dqCMqJCPFuvEt9YysaPUyjx2KkSYHVdxcx9MKFeM8noTDHM4lI74+oPqi3Yd8OloPP1wvK9vOruSjZzf7GJSNSPQFuNGWm2KIx8K4fV9LYB8vvGwX45sIABCWWid88EBxLLo5F/wM18ShTDJ1DcnPqjOUfErWw5RIbfjTvn1rdq6fFZmsutqVPIrBq29yNI7ou3xYBfGTw3ur8qEvolzToisOCSxfkpYDnlN+1ua1/tPEFZnNFRg2GACC2CoKZh/Q93rh9Zc6feZYT7vdM8sfKAGbemXtTAg1bALMOd9pIOyQmNXRK2j3Xkbg/Cnbjaa3nD2ceRl9toKpCiY1kR85Wm/cR6A3YZ3TcsN/9SARXYR+TvETb1x848WBXQQhFcA5+bZLXGhUmY/NKnm4yiA3DSmMI2J4ANE= jenkins@timmi-test"  ## invoice
+             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCTH8vZSHQ4OTaUlThDSGAuiXRGcXr9tn5jPbFWuWmzw4dcf0LicNbAhJ7wrSnyzI6v3FbnGvU3CRhVAfgXqtjV0V1okiDUwUqZqt/FpAnKhVwb0kei3xmqWcd4okULCx6HgSndoWOiIidAAd+ea776HqWu0UMqsFUB1FbHcjUlMp/UN3IIHLSMNN1iVZ+S4ijA6kpTHMfzXk3/is1gR6hawnqKriHWtuQwGHJ6Gz2O/DkekhEfqZ/poL1UKIuNNoF6xju9P1dKbpLg61TpQCMlfBLCuWUKgoDKmEcZNfD2LxWbJRJYcVu6y+ULPQuyygxZle+a+b+4qitZ67mdGRjBVbXbkVMy2wXLeq+TOGT7ISAC/bIvCSlTrsHN0xm5k8JBJs5O2CZtC3hD7Jfg2CTrBWXxKjL4TyY/OWnlRfbKbhDpg+j8MX+de6Fb8IFPNlA/AW70Ykyf/FH3rgxBvzUfA3tcRAO2rX3Nt5nsnwnh+53N6C38dvBnSKAxX6JdxaU= jenkins@timmi-test"  ## pro
+  ];
     repos = [
 
       { name = "nixos";
@@ -48,7 +53,7 @@
   security.sudo.extraRules = [
     {
       users = [ config.services.nix-deploy-git.user ];
-      commands = [ { command = "ALL"; options = [ "SETENV" "NOPASSWD" ]; } ];  ## could be limited to nixos-rebuild or nix-env
+      commands = [ { command = "ALL"; options = [ "SETENV" "NOPASSWD" ]; } ];  ## should be limited to nixos-rebuild or nix-env
     }
   ];
 }
