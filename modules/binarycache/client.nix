5869c4e5b1a754f237343eecc40e49b5fb354876
Johannes Lötzsch <git@johannesloetzsch.de>
2021-11-17 11:14:52 +0100

    hotfix binarycache

--- modules/binarycache/client.nix
+++ modules/binarycache/client.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
-
 {
+  ## during usage, you can still overwrite then:
+  ## > nix build --option substituters https://cache.nixos.org/
+  ## > nix build --option substitute false
+
   nix.binaryCaches = [ "https://binarycache.timmi.johannesloetzsch.de/" ];
   nix.binaryCachePublicKeys = [ "binarycache.timmi.johannesloetzsch.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];
 }
--- modules/binarycache/server.nix
+++ modules/binarycache/server.nix
@@ -4,8 +4,19 @@
     ../nginx/binarycache.nix
   ];
 
+  ## An alternative might be switching to https://github.com/thoughtpolice/eris
+
   services.nix-serve = {
     enable = true;
-    secretKeyFile = "/var/cache-priv-key.pem";  ## generated as described in https://nixos.wiki/wiki/Binary_Cache#1._Generating_a_private.2Fpublic_keypair
+
+    ## nix-serve fails with current nixos-unstable: https://github.com/edolstra/nix-serve/issues/28 when signing is enabled
+    #secretKeyFile = "/var/cache-priv-key.pem";  ## generated as described in https://nixos.wiki/wiki/Binary_Cache#1._Generating_a_private.2Fpublic_keypair
+
+    ## To get the correct permissions on the secretKeyFile, we may want use systemd.tmpfiles.rules:
+    ## https://github.com/Mic92/doctor-cluster-config/commit/303858529e02fd6f6a938dc7c76967cd410ed564#diff-8c0e51ff0928fd4e5f22f499d692a2e585e3f4adc3d24e866a7ea48eec26ca22L10
+    #systemd.tmpfiles.rules = [
+    #  "C /run/cache-priv-key.pem 400 nix-serve root - /var/cache-priv-key.pem"
+    #];
+
   };
 }
