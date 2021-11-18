6c89fb354bca1bd5d2ec1fdccd2c1aa1518088cf
System administrator <root@productive.timmi.johannesloetzsch.de>
2021-11-18 21:46:51 +0100

    fixed nginx default config

--- modules/nginx/common.nix
+++ modules/nginx/common.nix
@@ -6,11 +6,20 @@
   security.acme = {
     acceptTerms = true;
     email = "timmi_acme@johannesloetzsch.de";
+    preliminarySelfsigned = true;
     #server = "https://acme-staging-v02.api.letsencrypt.org/directory";
   };
 
   services.nginx = {
     enable = true;
+    recommendedTlsSettings = true;
+    recommendedOptimisation = true;
+    recommendedGzipSettings = true;
+    commonHttpConfig = ''
+      #types_hash_max_size 1024;
+      server_names_hash_bucket_size 128;
+    '';
+
     virtualHosts = {
       "${config.networking.fqdn}" = {
         forceSSL = true;
