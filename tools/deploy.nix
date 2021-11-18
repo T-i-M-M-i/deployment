a2b4198a1ef9252e5ad4b111b1ede3fc10cb989b
Johannes Lötzsch <git@johannesloetzsch.de>
2021-11-18 22:44:29 +0100

    added productive to tools/deploy.nix

--- tools/deploy.nix
+++ tools/deploy.nix
@@ -3,7 +3,7 @@
 let
   TEST="test.timmi.johannesloetzsch.de";
   STAGING="staging.timmi.johannesloetzsch.de";
-
+  PRODUCTIVE="productive.timmi.johannesloetzsch.de";
 in
 pkgs.writeScriptBin "timmi-nixos-deploy" ''
   #!${pkgs.runtimeShell}
@@ -17,13 +17,16 @@ pkgs.writeScriptBin "timmi-nixos-deploy" ''
   function setup_remotes() {
     git_remote_add deploy-test deploy@${TEST}:/var/lib/deploy/nixos.git
     git_remote_add deploy-staging deploy@${STAGING}:/var/lib/deploy/nixos.git
+    git_remote_add deploy-productive deploy@${PRODUCTIVE}:/var/lib/deploy/nixos.git
   }
 
   function deploy() {
     git push -u deploy-test master
     git push -u deploy-staging master
+    git push -u deploy-productive master
   }
 
   setup_remotes
   deploy
+  git fetch --all  ## when you have /etc/nixos for each server as remote, you will see the current state with `git log`
 ''
