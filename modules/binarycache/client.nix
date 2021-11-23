602c71acf73a7f89a5133e751a6c73d2c9c0fe16
Johannes Lötzsch <git@johannesloetzsch.de>
2021-11-23 20:20:31 +0100

    reactivated binaryCachePublicKey

--- modules/binarycache/client.nix
+++ modules/binarycache/client.nix
@@ -5,5 +5,5 @@
   ## > nix build --option substitute false
 
   nix.binaryCaches = [ "https://binarycache.timmi.johannesloetzsch.de/" ];
-  #nix.binaryCachePublicKeys = [ "binarycache.timmi.johannesloetzsch.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];
+  nix.binaryCachePublicKeys = [ "binarycache.timmi.johannesloetzsch.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];
 }
