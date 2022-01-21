6d9b0a451a8d23caa09a3bc243b5b7c29ead55ff
Johannes Lötzsch <git@johannesloetzsch.de>
2022-01-21 21:01:05 +0100

    fixed: nix.binaryCachePublicKeys

--- modules/binarycache/client.nix
+++ modules/binarycache/client.nix
@@ -5,5 +5,6 @@
   ## > nix build --option substitute false
 
   nix.binaryCaches = [ "https://binarycache.servers.timmitransport.de/" ];
-  nix.binaryCachePublicKeys = [ "binarycache.servers.timmitransport.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];
+  #nix.binaryCachePublicKeys = [ "binarycache.servers.timmitransport.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];
+  nix.binaryCachePublicKeys = [ "binarycache.timmi.johannesloetzsch.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];  ## url is part of the key
 }
