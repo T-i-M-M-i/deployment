8936a8af54725d8eb828a550eaaf59e882938540
Johannes Lötzsch <git@johannesloetzsch.de>
2023-01-07 23:22:30 +0100

    nix.binaryCaches instead of nix.settings.substituters

--- modules/binarycache/client.nix
+++ modules/binarycache/client.nix
@@ -4,6 +4,13 @@
   ## > nix build --option substituters https://cache.nixos.org/
   ## > nix build --option substitute false
 
-  nix.settings.substituters = [ "https://binarycache.servers.timmitransport.de/" ];
-  nix.settings.trusted-public-keys = [ "binarycache.timmi.johannesloetzsch.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];  ## url is part of the key
+  ## deprecated
+
+  nix.binaryCaches = [ "https://binarycache.servers.timmitransport.de/" ];
+  nix.binaryCachePublicKeys = [ "binarycache.timmi.johannesloetzsch.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];  ## url is part of the key
+
+  ## new
+
+  #nix.settings.substituters = [ "https://binarycache.servers.timmitransport.de/" ];
+  #nix.settings.trusted-public-keys = [ "binarycache.timmi.johannesloetzsch.de:YzP+MuSRG40O7H3EqFyq4+C7w9aKJXOWX1fkhmVCy/0=" ];  ## url is part of the key
 }
